9ee7145da365f3efdb4fa319d4b618ce01b9de94
SilentVibrant <silentvibrant@gmail.com>
2022-12-16 08:41:19 +0000

    adding in ufc events scraper class and adding relevant nuget packages for html parsing.

--- UfcFightCard/Program.cs
+++ UfcFightCard/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+               .AddJsonFile("C:\\Users\\Nafis Rahman\\source\\repos\\UpcomingMMAEvents\\UfcFightCard\\appsettings.json", false)
+               .Build();
+
+var ufcUpcomingEvents = "";
