eed6243fbad086e6334059f12a24ccf5a7b3ff4d
SilentVibrant <silentvibrant@gmail.com>
2022-12-17 14:26:13 +0000

    refactoring to remove hard coded strings from the helper logic and have them as constants in a separate file to improve readability and reduce chance of issues occuring from  accidental changes.

---
+++ UfcFightCard/Constants/Tags.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UfcFightCard.Constants
+{
+    public static class Tags
+    {
+        //Html Classes and Ids
+        public const string AthleteImage = "img.image-style-event-fight-card-upper-body-of-standing-athlete";
+        public const string GivenName = "span.c-listing-fight__corner-given-name";
+        public const string FamilyName = "span.c-listing-fight__corner-family-name";
+        public const string WeightClass = "div.c-listing-fight__class-text";
+        public const string Fights = "l-listing__group--bordered";
+        public const string Rows = "l-listing__item";
+        public const string Card = "c-card-event--result__logo";
+
+        //html tag constants
+        public const string Li = "li";
+        public const string Ul = "ul";
+        public const string Div = "div";
+        public const string A = "a";
+
+        //Attributes
+        public const string Class = "class";
+        public const string Href = "href";
+        public const string Src = "src";
+    }
+}
--- UfcFightCard/Helpers/HtmlNodeHelpers.cs
+++ UfcFightCard/Helpers/HtmlNodeHelpers.cs
@@ -1,5 +1,6 @@
 ﻿using HtmlAgilityPack;
 using ScrapySharp.Extensions;
+using UfcFightCard.Constants;
 
 namespace UfcFightCard.Helpers
 {
@@ -7,49 +8,49 @@ namespace UfcFightCard.Helpers
     {
         public static string LeftImage(this HtmlNode htmlNode)
         {
-            return htmlNode.CssSelect("img.image-style-event-fight-card-upper-body-of-standing-athlete").First().Attributes["src"].Value;
+            return htmlNode.CssSelect(Tags.AthleteImage).First().Attributes[Tags.Src].Value;
         }
         public static string RightImage(this HtmlNode htmlNode)
         {
-            return htmlNode.CssSelect("img.image-style-event-fight-card-upper-body-of-standing-athlete").ToList()[1].Attributes["src"].Value;
+            return htmlNode.CssSelect(Tags.AthleteImage).ToList()[1].Attributes[Tags.Src].Value;
         }
         public static string LeftName(this HtmlNode htmlNode)
         {
-            var givenName = htmlNode.CssSelect("span.c-listing-fight__corner-given-name").ToList()[0].InnerHtml;
-            var familyName = htmlNode.CssSelect("span.c-listing-fight__corner-family-name").ToList()[0].InnerHtml;
+            var givenName = htmlNode.CssSelect(Tags.GivenName).ToList()[0].InnerHtml;
+            var familyName = htmlNode.CssSelect(Tags.FamilyName).ToList()[0].InnerHtml;
             return $"{givenName} {familyName}";
         }
         public static string RightName(this HtmlNode htmlNode)
         {
-            var givenName = htmlNode.CssSelect("span.c-listing-fight__corner-given-name").ToList()[1].InnerHtml;
-            var familyName = htmlNode.CssSelect("span.c-listing-fight__corner-family-name").ToList()[1].InnerHtml;
+            var givenName = htmlNode.CssSelect(Tags.GivenName).ToList()[1].InnerHtml;
+            var familyName = htmlNode.CssSelect(Tags.FamilyName).ToList()[1].InnerHtml;
             return $"{givenName} {familyName}";
         }
         public static string WeightClass(this HtmlNode htmlNode)
         {
-            return htmlNode.CssSelect("div.c-listing-fight__class-text").First().InnerHtml;
+            return htmlNode.CssSelect(Tags.WeightClass).First().InnerHtml;
         }
         public static List<HtmlNode> Fights(List<HtmlNode> htmlNode)
         {
-            return htmlNode[0].Descendants("li")
-                .Where(node => node.GetAttributeValue("class", "").Contains("l-listing__item"))
+            return htmlNode[0].Descendants(Tags.Li)
+                .Where(node => node.GetAttributeValue(Tags.Class, "").Contains(Tags.Rows))
                 .ToList();
         }
         public static List<HtmlNode> UnorderedList(HtmlDocument htmlDoc)
         {
-            return htmlDoc.DocumentNode.Descendants("ul")
-                .Where(node => node.GetAttributeValue("class", "").Contains("l-listing__group--bordered"))
+            return htmlDoc.DocumentNode.Descendants(Tags.Ul)
+                .Where(node => node.GetAttributeValue(Tags.Class, "").Contains(Tags.Fights))
                 .ToList();
         }
         public static List<HtmlNode> LatestCard(HtmlDocument htmlDoc)
         {
-            return htmlDoc.DocumentNode.Descendants("div")
-                .Where(node => node.GetAttributeValue("class", "").Contains("c-card-event--result__logo"))
+            return htmlDoc.DocumentNode.Descendants(Tags.Div)
+                .Where(node => node.GetAttributeValue(Tags.Class, "").Contains(Tags.Card))
                 .ToList();
         }
         public static string MainCardUrl(List<HtmlNode> htmlNode)
         {
-            return htmlNode[0].SelectSingleNode("a").GetAttributeValue("href", "");
+            return htmlNode[0].SelectSingleNode(Tags.A).GetAttributeValue(Tags.Href, "");
         }
     }
 }
